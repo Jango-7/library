1b328747c099b87ee78c826787355d340e7176a6
unknown <jhoanngo2@gmail.com>
2025-04-25 18:21:26 -0300

    simplificacion

--- clases/biblioteca.py
+++ clases/biblioteca.py
@@ -1,5 +1,7 @@
 class Library:
     def __init__(self):
         self.avl_book_list = []
-        self.user_list = []
-        self.loan_list = []
+        #self.user_list = []
+        #self.loan_list = []
+    
+    #def register_books(self, title, ):
--- clases/libros.py
+++ clases/libros.py
@@ -1,9 +1,9 @@
 class Book:
-    def __init__(self, title, autor, isbn, available):
+    def __init__(self, title, autor, isbn):
         self.title = title
         self.autor = autor
         self.isbn = isbn
-        self.__available = available
+        #self.available = False
     
     def update_info(self, available=True):
         if available:
